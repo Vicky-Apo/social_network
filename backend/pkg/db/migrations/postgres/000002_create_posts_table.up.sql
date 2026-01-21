bd0534401ff54cd5ddf31a1bdd091abc0c7a3e7e
gpapadopoulos <ioannispap29@yahoo.gr>
2026-01-21 16:17:18 +0200

    add initial db migration

--- backend/pkg/db/migrations/postgres/000002_create_posts_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS posts (
-    id BIGSERIAL PRIMARY KEY,
-    author_id TEXT NOT NULL,
-    content TEXT NOT NULL,
-    privacy TEXT NOT NULL DEFAULT 'public' CHECK (privacy IN ('public','followers','custom')),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
-);
