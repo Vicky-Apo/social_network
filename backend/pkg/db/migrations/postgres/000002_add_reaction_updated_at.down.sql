73ba330cd58cafde3ae0cdb224a529afa8a3caaa
Smanousis <lookoptica@otenet.gr>
2026-02-08 15:42:07 +0200

    removed the backend folder to avoid conflicts

--- backend/pkg/db/migrations/postgres/000002_add_reaction_updated_at.down.sql
+++
@@ -1,7 +0,0 @@
--- Drop indexes
-DROP INDEX IF EXISTS idx_post_reactions_updated_at;
-DROP INDEX IF EXISTS idx_comment_reactions_updated_at;
-
--- Remove updated_at columns
-ALTER TABLE post_reactions DROP COLUMN IF EXISTS updated_at;
-ALTER TABLE comment_reactions DROP COLUMN IF EXISTS updated_at;
--- backend/pkg/db/migrations/sqlite/000002_create_posts_table.down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS idx_posts_privacy;
-DROP INDEX IF EXISTS idx_posts_author_id;
-DROP TABLE IF EXISTS posts;
